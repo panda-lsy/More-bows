ea812769f2c234369f0aa45f659c9ebea270e717
Ned Loynd <ned@loynd.com>
2023-05-18 17:48:38 +1000

    Merge client initialiser and projectile renderer

--- src/main/java/iDiamondhunter/morebows/Client.java
+++ src/main/java/iDiamondhunter/morebows/Client.java
@@ -1,15 +1,52 @@
 package iDiamondhunter.morebows;
 
+import iDiamondhunter.morebows.entities.CustomArrow;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.minecraft.client.item.ClampedModelPredicateProvider;
 import net.minecraft.client.item.ModelPredicateProviderRegistry;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EmptyEntityRenderer;
+import net.minecraft.client.render.entity.EntityRendererFactory.Context;
+import net.minecraft.client.render.entity.FlyingItemEntityRenderer;
+import net.minecraft.client.render.entity.ProjectileEntityRenderer;
+import net.minecraft.client.texture.SpriteAtlasTexture;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 
-/** The client mod initializer. */
-public final class Client implements ClientModInitializer {
+/** The client mod initializer + a custom EntityRenderer, to customize the rendering of frost arrows. */
+public final class Client extends ProjectileEntityRenderer<CustomArrow> implements ClientModInitializer {
+    private static final Identifier ARROWS = new Identifier("textures/entity/projectiles/arrow.png");
+
+    private final FlyingItemEntityRenderer<CustomArrow> snow;
+
+    @SuppressWarnings("NullAway")
+    public Client() {
+        super(new Context(null, null, null, null, null, null, null));
+        snow = null;
+    }
+
+    private Client(Context context) {
+        super(context);
+        snow = new FlyingItemEntityRenderer<>(context);
+    }
+
+    @Override
+    public Identifier getTexture(CustomArrow entity) {
+        return (entity.getDataTracker().get(CustomArrow.trackedType) == MoreBows.ARROW_TYPE_FROST) && !MoreBows.configGeneralInst.oldFrostArrowRendering ? SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE : ARROWS;
+    }
+
+    @Override
+    public void render(CustomArrow entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light) {
+        if ((entity.getDataTracker().get(CustomArrow.trackedType) == MoreBows.ARROW_TYPE_FROST) && !MoreBows.configGeneralInst.oldFrostArrowRendering) {
+            snow.render(entity, yaw, tickDelta, matrices, vertexConsumers, light);
+        } else {
+            // TODO Implement old cube rendering
+            super.render(entity, yaw, tickDelta, matrices, vertexConsumers, light);
+        }
+    }
+
     /** Client specific mod initialization code. */
     @Override
     public void onInitializeClient() {
@@ -18,7 +55,7 @@ public final class Client implements ClientModInitializer {
         final ClampedModelPredicateProvider pullProvider = (stack, world, entity, seed) -> ((entity == null) || (entity.getActiveItem() != stack) ? 0.0F : (stack.getMaxUseTime() - entity.getItemUseTimeLeft()) / ((CustomBow) stack.getItem()).powerDiv);
         final ClampedModelPredicateProvider pullingProvider = (stack, world, entity, seed) -> ((entity != null) && entity.isUsingItem() && (entity.getActiveItem() == stack) ? 1.0F : 0.0F);
         EntityRendererRegistry.register(MoreBows.ARROW_SPAWNER, EmptyEntityRenderer::new);
-        EntityRendererRegistry.register(MoreBows.CUSTOM_ARROW, CustomArrowRenderer::new);
+        EntityRendererRegistry.register(MoreBows.CUSTOM_ARROW, Client::new);
 
         for (final Item bow : MoreBows.getAllItems()) {
             ModelPredicateProviderRegistry.register(bow, pull, pullProvider);
